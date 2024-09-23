5a6386fdc2d798f94b6523d5a2706c39f4acd2d0
Mastervoliumpl <kubafijalkowski25@gmail.com>
2024-09-23 15:13:15 +0200

    Add project files.

---
+++ WPFSandbox/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFSandbox
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the startup window
+            MainWindow window = new MainWindow();
+            // Do stuff here, e.g. to the window
+            window.Title = "WPF Names";
+            // Show the window
+            window.Show();
+        }
+    }
+}
---
+++ WPFSandbox/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSandbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public List<string> lstNames { get; set; } = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearList() => lstNames.Items.Clear();
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text) && lstNames.Items.Count < 6)
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+            else
+            {
+                ClearList();
+            }
+        }
+
+        private void ButtonClearNames_Click(object sender, RoutedEventArgs e)
+        {
+            ClearList();
+        }
+
+        private void lstNames_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ClearList();
+        }
+
+        private void ButtonOptions_Click(object sender, RoutedEventArgs e)
+        {
+            Options options = new Options();
+            options.Show();
+        }
+    }
+}
